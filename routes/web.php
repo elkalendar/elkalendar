3a66d15686ffca868133aa0289afa259c2584aa7
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2024-08-02 09:04:54 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 require __DIR__ . '/app.php';
-//require __DIR__ . '/auth.php';
-//require __DIR__ . '/public.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/public.php';
 
