5dd27453c0b15b8c9833e25674f19443bc49fa25
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2024-09-09 12:15:40 +0300

    mvoe routes to common module

--- modules/Common/Routes/web.php
+++ modules/Common/Routes/web.php
@@ -3,8 +3,18 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Modules\Common\Http\Controllers\DashboardController;
+use Modules\Common\Http\Controllers\PagesController;
 use Modules\Common\Http\Controllers\TranslationController;
+
+Route::group([
+    'domain' => config('app.domain'),
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['web', 'localizationRedirect', 'localeViewPath'],
+], function () {
+    Route::get('/', [PagesController::class, 'home'])->name('home');
+});
 
 Route::group([
     'domain' => 'app.'.config('app.domain'),
