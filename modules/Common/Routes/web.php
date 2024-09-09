1b0fd037de5889c0a787c5dcb6203b9b353e7c1d
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2024-09-09 12:24:06 +0300

    move routes to web

--- modules/Common/Routes/web.php
+++ modules/Common/Routes/web.php
@@ -3,18 +3,8 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Modules\Common\Http\Controllers\DashboardController;
-use Modules\Common\Http\Controllers\PagesController;
 use Modules\Common\Http\Controllers\TranslationController;
-
-Route::group([
-    'domain' => config('app.domain'),
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['web', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
-], function () {
-    Route::get('/', [PagesController::class, 'home'])->name('home');
-});
 
 Route::group([
     'domain' => 'app.'.config('app.domain'),
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Modules\Common\Http\Controllers\PagesController;
+
+Route::group([
+    'domain' => config('app.domain'),
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['web', 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
+], function () {
+    Route::get('/', [PagesController::class, 'home'])->name('home');
+});
